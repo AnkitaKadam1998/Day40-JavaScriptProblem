16e45f25a25f3b90e9a5f9e38b24ea8c3f51db90
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-28 23:56:00 +0530

    Add: DisplayPlaceofdigit using javaScript

---
+++ DisplayPlaceOfDigit.js
@@ -0,0 +1,32 @@
+
+const readl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+readl.question("Enter numbers like 1,10,100,1000......100000000: ", (number) => {
+
+    if (number == 1){
+        console.log("Units");
+    } else if (number == 10) {
+        console.log("Ten's");
+    } else if (number == 100) {
+        console.log("Hundred's");
+    } else if (number == 1000) {
+        console.log("Thousand's");
+    }else if (number == 10000) {
+        console.log("Ten Thousand's");
+    }else if (number == 100000){
+        console.log("Lakh's");
+    }else if (number == 1000000){
+        console.log("Ten Lakh's");
+    }else if (number == 10000000){
+        console.log("Crore's");
+    } else if (number == 100000000){
+        console.log("Ten Crore's");
+    } else {
+        console.log("Invalid Number");
+    }
+
+    readl.close();
+});
